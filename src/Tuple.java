05dc8f1e5a87aad375406a89cdaff43b7691c695
jitendra sachdeva <jitendrs@thoughtworks.com>
2016-10-09 13:33:28 +0530

    Bit refactoring

--- src/Tuple.java
+++ src/Tuple.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
 
-public class Tuple {
-  public String[] states;
-  public Character[] alphabets;
-  public Transitions transitions;
-  public String initialState;
-  public ArrayList<String> finalStates;
+class Tuple {
+  private String[] states;
+  Character[] alphabets;
+  Transitions transitions;
+  String initialState;
+  ArrayList<String> finalStates;
 
-  public Tuple(String[] states, Character[] alphabets, Transitions transitions, String initialState, ArrayList<String> finalStates) {
+  Tuple(String[] states, Character[] alphabets, Transitions transitions, String initialState, ArrayList<String> finalStates) {
     this.states = states;
     this.alphabets = alphabets;
     this.transitions = transitions;
